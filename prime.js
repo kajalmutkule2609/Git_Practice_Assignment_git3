c0a2d1cc8b8bd622c1c884cfabd3a09530ac55c2
unknown <pujacse20@gmail.com>
2023-02-15 17:55:37 +0530

    Correction Done

--- prime.js
+++ prime.js
@@ -1,13 +1,16 @@
 let num=13;
 let temp=0;
 for(let i=2;i<num;i++){
-if(num%i==0){
+if(num%i==0)
+{
 temp++;
 }
 }
-if(temp==0){
-console.log("Not Prime");
+if(temp==0)
+{
+console.log("This is a Prime number");
 }
-else{
-console.log("Prime");
+else
+{
+console.log("This is not a Prime number");
 }
